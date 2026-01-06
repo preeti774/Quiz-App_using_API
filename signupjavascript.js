70824f2e221184e3e54f24d729a00829923cc803
preeti774 <preeti5026634@gmail.com>
2026-01-06 19:52:40 +0530

    Update signupjavascript.js

--- signupjavascript.js
+++ signupjavascript.js
@@ -1,61 +1,87 @@
-const signbutt=document.querySelector("#signup")
-const name=document.querySelector("#name")
+const name=document.querySelector('#name')
 const email=document.querySelector("#emailname")
-const password=document.querySelector("#passwordname")
-const conpass=document.querySelector("#confirmname")
-const passtext=document.querySelector(".text2")
-const nametext=document.querySelector(".name1")
-const emailtext=document.querySelector(".text")
-const conpasstext=document.querySelector(".text3")
-signbutt.addEventListener("click",function(e){
-  e.preventDefault();
-    emailtext.innerHTML=""
-    nametext.innerHTML=""
-   passtext.innerHTML=""
-   conpasstext.innerHTML=""
-   password.style.border="2px solid black"
-   conpass.style.border="2px solid black"
-name.style.border="2px solid black"
-email.style.border="2px solid black"
-   const regexname=/^[A-Za-z\s]+$/;
-   const regexemail=/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-  const username=name.value.trim();
+const pass=document.querySelector("#passwordname")
+const confirms=document.querySelector("#confirmname")
+const signup=document.querySelector("#signup")
+const name1=document.querySelector(".name1")
+const name2=document.querySelector(".text")
+const name3=document.querySelector(".text2")
+const name4=document.querySelector(".text3")
+signup.addEventListener("click",function(e){
+  e.preventDefault()
+  const first=[name1,name2,name3,name4]
+  const second=[name,email,pass,confirms]
+  first.forEach((l)=>{
+    l.innerHTML=""
+  })
+  second.forEach((l)=>{
+    l.style.border="2px solid black"
+  })
+  const username=name.value.trim()
+  if(username.length<3||username===""){
+   name1.innerHTML="please enter a valid name";
+   name.style.border="2px solid red"
+   return;
+  }
   const useremail=email.value.trim().toLowerCase();
-  const userpass=password.value.trim();
-  const userconpass=conpass.value.trim();
-  if(username===""||useremail===""||userpass===""||userconpass===""){
-    alert("Please enter all details")
+  const regex=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if(useremail===""){
+   name2.innerHTML="please enter email"
+   email.style.border="2px solid red"
+   return;
   }
-  else if((!regexname.test(username))||username.length<3){
-nametext.innerHTML="Please enter valid name"
-name.style.border="2px solid red"
+  else if(!regex.test(useremail)){
+  name2.innerHTML="please enter valid email format"
+   email.style.border="2px solid red"
+   return ;
   }
-  else if(!regexemail.test(useremail)){
- emailtext.innerHTML="Plese enter valid email format"
- email.style.border="2px solid red"
+  const userpass=pass.value
+  const regexpass=/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/
+  if(userpass===""){
+    name3.innerHTML="please enter password";
+   pass.style.border="2px solid red"
+   return;
   }
-  else if(!useremail.endsWith("@gmail.com")){
-    emailtext.innerHTML="Plese enter a valid @gmail.com address"
- email.style.border="2px solid red"
+  else if(userpass.length<8){
+    name3.innerHTML="please enter atleast 8 character";
+   pass.style.border="2px solid red"
+   return;
   }
-   else if(userpass.length<6){
-passtext.innerHTML="Please enter atleast 6 characters"
-password.style.border="2px solid red"
+  else if(!regexpass.test(userpass)){
+    name3.innerHTML="please enter atleast one character and one number";
+   pass.style.border="2px solid red"
+   return;
   }
- else if(userpass!==userconpass){
-conpasstext.innerHTML="Please enter correct password"
-conpass.style.border="2px solid red"
+  const userconfirm=confirms.value
+  if(userconfirm===""){
+   name4.innerHTML="please enter confirm password";
+  confirms.style.border="2px solid red"
+  return;
+  }
+  else if(userconfirm!=userpass){
+    name4.innerHTML="please enter correct password";
+  confirms.style.border="2px solid red"
+  return;
+  } 
+  if(name1.innerHTML===""&&name2.innerHTML===""&&name3.innerHTML===""&&name4.innerHTML==="")
+  {
+    const localdata=JSON.parse(localStorage.getItem("key"))||[];
+  const final=localdata.find((l)=>(useremail===l.email))
+  if(final){
+    alert("This email is already Registered ! Please login")
+    return;
+  }
+  else{
+    const userobj={
+      email:useremail,
+      password:userpass
+    }
+    localdata.push(userobj)
+    localStorage.setItem("key",JSON.stringify(localdata))
+    alert("Signup successfully")
+    second.forEach((l)=>{
+      l.value="";
+    })
+  }
 }
-else{
-   auth.createUserWithEmailAndPassword(useremail,userpass)
-   .then((userCredential)=>{
-    userCredential.user.sendEmailVerification().then(()=>{
-     alert("Signup successfully ! ek link bhja hai us pr confirm krlo")
-     window.location.href="index.html";
-    })
-   })
-    .catch((error)=>{
-   alert("Error"+error)
-    })
-  }
 })
